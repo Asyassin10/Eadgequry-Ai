97a4ac49adee04e4a9fcf0a3085c94b9ffeb9c73
Asyassin10 <asyasson07@gmail.com>
2025-11-04 16:39:38 +0100

    add notifcation

---
+++ notification/src/test/java/com/eadgequry/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eadgequry.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
